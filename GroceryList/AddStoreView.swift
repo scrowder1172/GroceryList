150f325d7265c233d176f6b2b9eca60774be61b4
Scott Crowder <scrowder@gmail.com>
2023-11-21 11:20:10 -0600

    add file

---
+++ GroceryList/AddStoreView.swift
@@ -0,0 +1,18 @@
+//
+//  AddStoreView.swift
+//  GroceryList
+//
+//  Created by SCOTT CROWDER on 11/21/23.
+//
+
+import SwiftUI
+
+struct AddStoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddStoreView()
+}
