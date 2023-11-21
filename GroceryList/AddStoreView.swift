0cba44c00a908aad950a568128239d45d5c9a3b8
Scott Crowder <scrowder@gmail.com>
2023-11-21 11:43:33 -0600

    create AddStore workflow

--- GroceryList/AddStoreView.swift
+++ GroceryList/AddStoreView.swift
@@ -6,10 +6,36 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct AddStoreView: View {
+    
+    @State private var storeName: String = ""
+    
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                TextField("store", text: $storeName)
+            }
+            .navigationTitle("Add Store")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                Button("Save") {
+                    let newStore: Stores = Stores(storeName: storeName)
+                    modelContext.insert(newStore)
+                    dismiss()
+                }
+            }
+        }
+    }
+    
+    func validateForm() -> Bool {
+        guard storeName.isNotEmpty else { return false }
+        
+        return true
     }
 }
 
