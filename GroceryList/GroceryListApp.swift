43c0c630152a01a8c178aa2f6da6f805cffda0c0
Scott Crowder <scrowder@gmail.com>
2023-11-21 11:05:42 -0600

    Initial Commit

---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by SCOTT CROWDER on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
