31e206230e3b2d034b54a2138ba6ce30b49f9798
Scott Crowder <scrowder@gmail.com>
2023-11-21 11:15:09 -0600

    create model for Stores

--- GroceryList/DataModels.swift
+++ GroceryList/DataModels.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model 
+final class Stores {
+    var store: String
+    let dateAdded: Date = Date()
+    var formattedDateAdded: String {
+        dateAdded.formatted(date: .long, time: .standard)
+    }
+    
+    init(store: String) {
+        self.store = store
+    }
+}
