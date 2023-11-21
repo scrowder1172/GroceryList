a5fbe9b456cdade40f3f34969988f9d908f267a9
Scott Crowder <scrowder@gmail.com>
2023-11-21 11:42:51 -0600

    add store container

--- GroceryList/GroceryListApp.swift
+++ GroceryList/GroceryListApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct GroceryListApp: App {
@@ -13,5 +14,6 @@ struct GroceryListApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: [Stores.self])
     }
 }
